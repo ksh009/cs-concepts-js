3d229e30d07d6b08dee9133fae78931dfcc299e5
HughesSeanKyle <khughessean@yahoo.com>
2023-05-05 20:22:36 +0200

    Set example

---
+++ 8.Singly_Linked_List_Set.js
@@ -0,0 +1,103 @@
+/*
+    Set
+    Changing the value of a node based on it's position in the Linked List
+        This function should accept a value and an index
+        Use your get function to find the specific node.
+        If the node is not found, return false
+        If the node is found, set the value of that node to be the value passed to the function and return true
+*/
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+	push(val) {
+		var newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
+	pop() {
+		if (!this.head) return undefined;
+		var current = this.head;
+		var newTail = current;
+		while (current.next) {
+			newTail = current;
+			current = current.next;
+		}
+		this.tail = newTail;
+		this.tail.next = null;
+		this.length--;
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+		return current;
+	}
+	shift() {
+		if (!this.head) return undefined;
+		var currentHead = this.head;
+		this.head = currentHead.next;
+		this.length--;
+		if (this.length === 0) {
+			this.tail = null;
+		}
+		return currentHead;
+	}
+	unshift(val) {
+		var newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		}
+		newNode.next = this.head;
+		this.head = newNode;
+		this.length++;
+		return this;
+	}
+	get(index) {
+		if (index < 0 || index >= this.length) return null;
+		var counter = 0;
+		var current = this.head;
+		while (counter !== index) {
+			current = current.next;
+			counter++;
+		}
+		return current;
+	}
+	set(index, val) {
+		// This function should accept a value and an index
+		// Use your get function to find the specific node.
+		var foundNode = this.get(index);
+		// If the node is found, set the value of that node to be the value passed to the function and return true
+		if (foundNode) {
+			foundNode.val = val;
+			return true;
+		}
+		// If the node is not found, return false
+		return false;
+	}
+}
+
+var list = new SinglyLinkedList();
+
+list.push('HELLO');
+list.push('GOODBYE');
+list.push('!');
+list.push('<3');
+list.push(':)');
