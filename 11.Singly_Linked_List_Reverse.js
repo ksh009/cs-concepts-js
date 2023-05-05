d685d1422706be767c1ce0cef4a16286c20fc531
HughesSeanKyle <khughessean@yahoo.com>
2023-05-05 20:51:55 +0200

    Reverse example

---
+++ 11.Singly_Linked_List_Reverse.js
@@ -0,0 +1,169 @@
+/*
+    REVERSE
+        Reversing the Linked List
+
+        in place!
+
+        Swap the head and tail
+        Create a variable called next
+        Create a variable called prev
+        Create a variable called node and initialize it to the head property
+        Loop through the list
+        Set next to be the next property on whatever node is
+        Set the next property on the node to be whatever prev is
+        Set prev to be the value of the node variable
+        Set the node variable to be the value of the next variable
+        Once you have finished looping, return the list
+*/
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+	push(val) {
+		var newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
+	pop() {
+		if (!this.head) return undefined;
+		var current = this.head;
+		var newTail = current;
+		while (current.next) {
+			newTail = current;
+			current = current.next;
+		}
+		this.tail = newTail;
+		this.tail.next = null;
+		this.length--;
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+		return current;
+	}
+	shift() {
+		if (!this.head) return undefined;
+		var currentHead = this.head;
+		this.head = currentHead.next;
+		this.length--;
+		if (this.length === 0) {
+			this.tail = null;
+		}
+		return currentHead;
+	}
+	unshift(val) {
+		var newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		}
+		newNode.next = this.head;
+		this.head = newNode;
+		this.length++;
+		return this;
+	}
+	get(index) {
+		if (index < 0 || index >= this.length) return null;
+		var counter = 0;
+		var current = this.head;
+		while (counter !== index) {
+			current = current.next;
+			counter++;
+		}
+		return current;
+	}
+	set(index, val) {
+		var foundNode = this.get(index);
+		if (foundNode) {
+			foundNode.val = val;
+			return true;
+		}
+		return false;
+	}
+	insert(index, val) {
+		if (index < 0 || index > this.length) return false;
+		if (index === this.length) return !!this.push(val);
+		if (index === 0) return !!this.unshift(val);
+
+		var newNode = new Node(val);
+		var prev = this.get(index - 1);
+		var temp = prev.next;
+		prev.next = newNode;
+		newNode.next = temp;
+		this.length++;
+		return true;
+	}
+	remove(index) {
+		if (index < 0 || index >= this.length) return undefined;
+		if (index === 0) return this.shift();
+		if (index === this.length - 1) return this.pop();
+		var previousNode = this.get(index - 1);
+		var removed = previousNode.next;
+		previousNode.next = removed.next;
+		this.length--;
+		return removed;
+	}
+
+	/*
+        Reverse in place 
+            - Traverse and as you go you reverse things (Note C reverse algo example)
+    */
+	reverse() {
+		// Swap the head and tail
+		// Create a variable called next
+		// Create a variable called prev
+		// Create a variable called node and initialize it to the head property
+		var node = this.head;
+		this.head = this.tail;
+		this.tail = node;
+		var next;
+		var prev = null;
+		// Loop through the list
+		// Set next to be the next property on whatever node is
+		// Set the next property on the node to be whatever prev is
+		// Set prev to be the value of the node variable
+		// Set the node variable to be the value of the next variable
+		for (var i = 0; i < this.length; i++) {
+			next = node.next;
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+		// Once you have finished looping, return the list
+		return this;
+	}
+	print() {
+		var arr = [];
+		var current = this.head;
+		while (current) {
+			arr.push(current.val);
+			current = current.next;
+		}
+		console.log(arr);
+	}
+}
+
+var list = new SinglyLinkedList();
+
+list.push(100);
+list.push(201);
+list.push(250);
+list.push(350);
+list.push(999);
