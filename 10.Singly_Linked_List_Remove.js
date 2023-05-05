c1ba0e5c38b6101b9bb212da9b5ec41afff36c74
HughesSeanKyle <khughessean@yahoo.com>
2023-05-05 20:43:02 +0200

    Remove example

---
+++ 10.Singly_Linked_List_Remove.js
@@ -0,0 +1,136 @@
+/*
+    Remove
+        Removing a node from the Linked List at a specific position
+
+            If the index is less than zero or greater than the length, return undefined
+            If the index is the same as the length-1, pop
+            If the index is 0, shift
+            Otherwise, using the get method, access the node at the index - 1
+            Set the next property on that node to be the next of the next node
+            Decrement the length
+            Return the value of the node removed
+*/
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+	push(val) {
+		var newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
+	pop() {
+		if (!this.head) return undefined;
+		var current = this.head;
+		var newTail = current;
+		while (current.next) {
+			newTail = current;
+			current = current.next;
+		}
+		this.tail = newTail;
+		this.tail.next = null;
+		this.length--;
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+		return current;
+	}
+	shift() {
+		if (!this.head) return undefined;
+		var currentHead = this.head;
+		this.head = currentHead.next;
+		this.length--;
+		if (this.length === 0) {
+			this.tail = null;
+		}
+		return currentHead;
+	}
+	unshift(val) {
+		var newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		}
+		newNode.next = this.head;
+		this.head = newNode;
+		this.length++;
+		return this;
+	}
+	get(index) {
+		if (index < 0 || index >= this.length) return null;
+		var counter = 0;
+		var current = this.head;
+		while (counter !== index) {
+			current = current.next;
+			counter++;
+		}
+		return current;
+	}
+	set(index, val) {
+		var foundNode = this.get(index);
+		if (foundNode) {
+			foundNode.val = val;
+			return true;
+		}
+		return false;
+	}
+	insert(index, val) {
+		if (index < 0 || index > this.length) return false;
+		if (index === this.length) return !!this.push(val);
+		if (index === 0) return !!this.unshift(val);
+
+		var newNode = new Node(val);
+		var prev = this.get(index - 1);
+		var temp = prev.next;
+		prev.next = newNode;
+		newNode.next = temp;
+		this.length++;
+		return true;
+	}
+	/*
+        Remove list at specific idx and patches up list around it 
+        Method for removing from the middle for example 
+    */
+	remove(index) {
+		// If the index is less than zero or greater than the length, return undefined
+		if (index < 0 || index >= this.length) return undefined;
+		// If the index is the same as the length-1, pop
+		// If the index is 0, shift
+		if (index === 0) return this.shift();
+		if (index === this.length - 1) return this.pop();
+		// Otherwise, using the get method, access the node at the index - 1
+		// Set the next property on that node to be the next of the next node
+		var previousNode = this.get(index - 1);
+		var removed = previousNode.next;
+		previousNode.next = removed.next;
+		// Decrement the length
+		this.length--;
+		// Return the value of the node removed
+		return removed;
+	}
+}
+
+var list = new SinglyLinkedList();
+
+list.push(100);
+list.push(201);
+list.push(250);
+list.push(350);
